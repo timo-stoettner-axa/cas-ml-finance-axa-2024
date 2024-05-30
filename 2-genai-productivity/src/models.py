edf610286fa58ca770f956c80f600f42832d4b67
Timo Stöttner <mail@timo-stoettner.de>
2024-05-30 11:49:50 +0200

    Better support for Google Colab

--- 2-genai-productivity/src/models.py
+++ 2-genai-productivity/src/models.py
@@ -1,13 +1,4 @@
-import os
-
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-
-try:
-    from google.colab import userdata
-    os.environ["OPENAI_API_KEY"] = userdata.get('OPENAI_API_KEY')
-except ImportError:
-    # Not executed in Google Colab
-    pass
 
 gpt35 = ChatOpenAI(model_name="gpt-3.5-turbo")
 gpt4 = ChatOpenAI(model_name="gpt-4o")
