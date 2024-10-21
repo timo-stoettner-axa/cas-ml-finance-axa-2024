b671ad5d27cd2a0f1eb1b380042c58064e14a6fc
Timo Stöttner <mail@timo-stoettner.de>
2024-10-21 09:19:25 +0200

    Changed model to gpt-4o-mini

--- 2-genai-productivity/src/models.py
+++ 2-genai-productivity/src/models.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-gpt35 = ChatOpenAI(model_name="gpt-3.5-turbo")
+gpt4o_mini = ChatOpenAI(model_name="gpt-4o-mini")
 gpt4 = ChatOpenAI(model_name="gpt-4o")
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
